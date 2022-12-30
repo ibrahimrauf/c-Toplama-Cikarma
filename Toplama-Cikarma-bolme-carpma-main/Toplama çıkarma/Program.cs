ba91d3505244fdab9a1ff86465744d6509b5a239
ibrahimrauf <119485615+ibrahimrauf@users.noreply.github.com>
2022-12-30 23:05:33 +0300

    Add files via upload

---
+++ Toplama-Cikarma-bolme-carpma-main/Toplama çıkarma/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+float sayi1, sayi2, topla, cikar, carp, bol;
+Console.Write("1. Sayiyi giriniz");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2. sayiyi giriniz");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+topla = sayi1 + sayi2;
+cikar = sayi1 - sayi2;
+carp = sayi1 * sayi2;
+bol = sayi1 / sayi2;
+
+
+
+Console.WriteLine("Toplama Sonucu : " + topla  );
+Console.WriteLine("Cikarma Sonucu : " + cikar );
+Console.WriteLine("Carpma Sonucu : " + carp );
+Console.WriteLine("Bolme Sonucu : " + bol );
+
+
